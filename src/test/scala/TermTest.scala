85d81573e7d9a307535306fa13d32115ee061594
Martin <marty@logic.at>
2015-04-23 18:51:40 +0200

    added replacement test

--- src/test/scala/TermTest.scala
+++ src/test/scala/TermTest.scala
@@ -8,24 +8,65 @@ import org.mockito.Matchers._
 @RunWith( classOf[JUnitRunner] )
 class TermTest extends SpecificationWithJUnit {
   "The term library" should {
-    "create basic terms with the parser" in {
-      val tl = new TermList
-      val z = Var("z", tl)
+    val tl = new TermList
 
-      val x = LambdaVar(0, tl)
-      val y = LambdaVar(1, tl)
+    //shortcuts
+    val app = tl.createApp(_,_)
+    val freevar = tl.createVar(_)
+    val boundvar = tl.createLambdaVar(_)
+    val lambda = tl.createLambda(_)
 
-      val lxzx = Lambda(App(z,x, tl),tl)
-      val lyxy = Lambda(Lambda(y,tl), tl)
+
+    "create basic terms" in {
+      val z = freevar("z")
+
+      val x = boundvar(0)
+      val y = boundvar(1)
+
+      val lxzx = lambda(app(z,x))
+      val lyxy = lambda(lambda(y))
+      val lxx = lambda(x)
 
       println(lxzx)
       println(lyxy)
 
-      
+      val app1 = app(lxzx, z)
+      println(app1)
 
       ok("done")
     }
+
+    "perform replacement" in {
+      val z = freevar("z")
+
+      val x = boundvar(0)
+
+      val lxzx = lambda( app(z, x) )
+      val lxx = lambda(x)
+
+      val r1 = lxzx.replace(lxx, List(0,0))
+
+
+      println(r1)
+
+      //check if the term is like we expected
+      val expect1 = lambda(app(lambda(x),x))
+      r1 must_== expect1
+
+      // check if the db-Indices are always 0, since they refer to the next binder
+      r1 match {
+        case Lambda(
+               App(Lambda(LambdaVar(x,_),_),
+                   LambdaVar(y,_),
+                   _),
+               _
+             ) =>
+               x must_== 0
+               y must_== 0
+        case _ =>
+          ko("result is not of shape Lambda(App(Lambda(LambdaVar(_), LambdaVar(_)))!")
+      }
+
+    }
   }
 }
-
-
